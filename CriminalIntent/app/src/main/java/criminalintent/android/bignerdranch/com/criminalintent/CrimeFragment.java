ea4cb654bd9176e70b6c2aa8c235d144c7889173
groz <tagir.magomedov@gmail.com>
2016-03-17 01:20:21 -0700

    Inflate crime fragment and set listeners

--- CriminalIntent/app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeFragment.java
@@ -1,13 +1,21 @@
 package criminalintent.android.bignerdranch.com.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
     private static String TAG = "CrimeFragment";
 
     private Crime mCrime;
+    private EditText mCrimeTitle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,31 @@ public class CrimeFragment extends Fragment {
         Log.d(TAG, "onCreate");
 
         mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mCrimeTitle = (EditText) v.findViewById(R.id.crime_title);
+        mCrimeTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        return v;
     }
 
     @Override
