840b5769f820f6abc22a87f0cc8abc9084523dcc
groz <tagir.magomedov@gmail.com>
2016-03-17 01:11:11 -0700

    Add logging on state changes

--- CriminalIntent/app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeFragment.java
@@ -2,14 +2,54 @@ package criminalintent.android.bignerdranch.com.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
 public class CrimeFragment extends Fragment {
+    private static String TAG = "CrimeFragment";
 
     private Crime mCrime;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+
         mCrime = new Crime();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 }
--- CriminalIntent/app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CriminalActivity.java
+++ CriminalIntent/app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CriminalActivity.java
@@ -2,12 +2,51 @@ package criminalintent.android.bignerdranch.com.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 public class CriminalActivity extends FragmentActivity {
+    private static String TAG = "CriminalActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criminal);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 }
