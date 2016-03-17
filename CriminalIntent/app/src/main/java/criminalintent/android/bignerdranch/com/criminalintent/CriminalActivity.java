ae9b414c61fb4e2d66d863658aa7cf55f5035992
groz <tagir.magomedov@gmail.com>
2016-03-17 00:37:06 -0700

    Chapter 7. Init fragment activity

---
+++ CriminalIntent/app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CriminalActivity.java
@@ -0,0 +1,13 @@
+package criminalintent.android.bignerdranch.com.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class CriminalActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criminal);
+    }
+}
