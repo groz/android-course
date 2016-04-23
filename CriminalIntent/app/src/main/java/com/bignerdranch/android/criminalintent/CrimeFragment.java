0719cbb9f4421f00bcb949bcd1566d0996fa61ba
Tagir Magomedov <tagir@uber.com>
2016-04-22 23:45:53 -0700

    Chapter 15. Challenge 1, create send Intent with ShareCompat

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -192,10 +193,12 @@ public class CrimeFragment extends Fragment {
         mReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_TEXT, generateCrimeReport());
-                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
+                Intent i = ShareCompat.IntentBuilder.from(getActivity())
+                        .setSubject(getString(R.string.crime_report_subject))
+                        .setText(generateCrimeReport())
+                        .setType("text/plain")
+                        .getIntent();
+
                 i = Intent.createChooser(i, getString(R.string.send_report));
                 startActivity(i);
             }
